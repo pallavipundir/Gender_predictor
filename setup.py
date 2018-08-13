8b2f322daa19332c76243beab8d11e07ac2ff633
Pallavi Pundir <30193185+pallavipundir@users.noreply.github.com>
2018-08-13 13:38:10 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='Gender_predictor',
+      version='0.1',
+      description=(' '),
+      author='Pallavi Pundir',
+      author_email='pallavi.pundir18@gmail.com',
+      url='https://github.com/pallavipundir/Gender_predictor',
+      packages=find_packages(),
+      install_requires=['nltk'],
+      )
